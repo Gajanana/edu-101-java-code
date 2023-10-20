d401a9ff820e1f4ce752444b90330b3314f00072
Gajanana <gajukorse.hegde80@gmail.com>
2023-10-20 07:36:29 +0000

    chage iterface

--- samples/greeting/src/main/java/greeting/GreetingImpl.java
+++ samples/greeting/src/main/java/greeting/GreetingImpl.java
@@ -1,8 +1,12 @@
 package greeting;
-
+import io.temporal.workflow.WorkflowInterface
+import io.temporal.workflow.WorkflowMethod
+ @WorkflowInterface
 public class GreetingImpl implements Greeting {
 
     @Override
+   
+    @WorkflowMethod
     public String greetSomeone(String name) {
         return "Hello " + name + "!";
     }
